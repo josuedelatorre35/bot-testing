a94b4fef67a6e050879e4b5679a9249a85508cbe
Josue De La Torre <josuedelatorre35@yahoo.com>
2022-03-17 15:53:42 -0700

    commit on 'main

--- main.py
+++ main.py
@@ -43,5 +43,32 @@ async def on_message(message):
 async def clear(ctx, amount = 100):
     await ctx.channel.purge(limit=amount + 1) #use +1 to also delete the $clear message
 
+@client.command()
+async def ping(ctx):
+  await ctx.send(f'Pong! {round(client.latency * 1000)}ms' )
+
+@client.command()
+async def kick(ctx, member : discord.Member, *, reason=None):
+    await member.kick(reason=reason) 
+
+@client.command()
+async def ban(ctx, member : discord.Member, *, reason=None):
+    await member.ban(reason=reason) 
+    await ctx.send(f'Banned {member.mention}')
+
+@client.command()
+async def unban(ctx, *, member):
+    banned_users = await ctx.guild.bans()
+    member_name, member_discriminator = member.split('#')
+
+    for ban_entry in banned_users:
+        user = ban_entry.user
+        
+        if (user.name, user.discriminator) == (member_name, member_discriminator):
+            await ctx.guild.unban(user)
+            await ctx.send(f'Unbanned {user.mention}')
+            return
+
+
 
 client.run(mytokens.TOKEN) #runs on_message definition  
